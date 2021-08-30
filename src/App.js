f07aa7bc4460846f9697c97dca977c543621537f
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2021-08-30 14:06:07 -0400

    Add files via upload
    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Coin from "./Coin";
-
 import "./App.css";
+import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -11,12 +10,12 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=250&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.error(error));
   }, []);
 
   const handleChange = (e) => {
@@ -28,12 +27,13 @@ function App() {
   );
 
   return (
-    <div className="coin-app">
+    <div className="container coin-app">
       <div className="coin-search">
-        <h1 className="coin-text">Search a currency</h1>
+        <div className="row">
+          <h1 className="col-md-12 coin-text">Search a Currency</h1>
+        </div>
         <form>
           <input
-            type="text"
             placeholder="Search"
             className="coin-input"
             onChange={handleChange}
@@ -45,12 +45,12 @@ function App() {
           <Coin
             key={coin.id}
             name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.total_volume}
-            volume={coin.market_cap}
             image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.total_volume}
+            price={coin.current_price}
             priceChange={coin.price_change_percentage_24h}
+            marketCap={coin.market_cap}
           />
         );
       })}
